249e50608793193ac4e0c7c0b7ac66cc65a85858
Coolbeans51 <163181492+Coolbeans51@users.noreply.github.com>
2024-07-27 14:44:26 -0400

    Commit: Fixed dialogue
    Fixed dialogue to allow for reading multiple lines and disappearing when finished

--- Assets/Scenes/Dialogo/Dialogue.cs
+++ Assets/Scenes/Dialogo/Dialogue.cs
@@ -21,20 +21,47 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
     }
 
     void StartDialogue(){
         index = 0;
         StartCoroutine(TypeLine());
+        // If the player clicks the mouse and the lines array is not out of lines, the program prints a line from the line array
+        // It also sets the index to the next line in the array
+        
     }
 
     IEnumerator TypeLine(){
 
-        // Type each character 1 by 1
+        // Type each character from a line one at a time
         foreach (char c in lines[index].ToCharArray()){
             textComponent.text += c;
             yield return new WaitForSeconds(textSpeed);
+        }
+    }
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
         }
     }
 }
