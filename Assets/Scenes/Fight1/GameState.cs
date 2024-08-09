7dab4b98d0c189895732905211da713835b0d966
Coolbeans51 <163181492+Coolbeans51@users.noreply.github.com>
2024-08-09 14:36:27 -0400

    More addition

--- Assets/Scenes/Fight1/GameState.cs
+++ Assets/Scenes/Fight1/GameState.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public enum BattleState { START, PLAYER, ENEMY, WIN, LOSS };
+public enum BattleState { START, PLAYER1TURN, PLAYER2TURN, ENEMY, WIN, LOSS };
 
 public class GameState : MonoBehaviour
 {
-    public GameObject colin;    // Character game objects
-    public GameObject brandon;
-    public GameObject brian;
-    public GameObject bully1;
-    public GameObject bully2;
+    public Person protagonists; // Character game objects
+    public Person[] antagonists;
+
+    public Button colinSelect;
+    public Button brandonSelect;
+    public Button[] colinButtons;
+    public Button[] brandonButtons;
+
+
 
     public Text battleDisplay; // Battle display
 
@@ -22,12 +26,35 @@ public class GameState : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        state = BattleState.PLAYER;
-
+        state = BattleState.START;
+        BattleSetup();
+        
         
     }
+    void BattleSetup()
+    {
+        battleDisplay.text = "Battle Start!";
+        state = BattleState.PLAYER1TURN;
+        Player1Turn();
+    }
 
-    
+    void Player1Turn()
+    {
+        battleDisplay.text = "Choose your first attacker:";
+        colinSelect.gameObject.SetActive(true);
+        brandonSelect.gameObject.SetActive(true);
+
+    }
+
+    public void ColinFirst()
+    {
+        colinSelect.gameObject.SetActive(false);
+        brandonSelect.gameObject.SetActive(false);
+        foreach (Button attack in colinButtons)
+        {
+            attack.gameObject.SetActive(true);
+        }
+    }
 
     //add 
     // Update is called once per frame
